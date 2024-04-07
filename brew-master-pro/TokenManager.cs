c0a651dd003780797bc550c2ebafb38ae9010d50
Usman Afzal Khan <usmanafzalkhan01@gmail.com>
2024-04-07 07:32:39 -0400

    feat: add JWT for email and role

---
+++ brew-master-pro/TokenManager.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Web;
+
+namespace brew_master_pro
+{
+    public class TokenManager
+    {
+        public static string Secret = "pucsyodirqbzjevwomtkdnrzifbxpgvmxfwrsedihpnaorvctejxyuwnkbmxyagcmtldwqjlhwmxbkrrkxqcsyvpfhjebzwtvmsoytidzbwqmctupjxysvspwltcjzixteiqkgnxruubayxfsvlcikcxnwrytueogmxlnrzhrqkjvctwzedozrpvcwnmfyvoztbnulxgzldrpakbhtqjagvienwdqsybifrnlnmsvrtxkorwxjedqbnfzgqpjxdmhoajnzrmtoxdybpmfxqwakscudljgtmrpuomhzfsnkviyqenbmwtzqaxrthvfknbwusdlkajecqfsvzjxomgavkqyxnzjxqrzdngoyluekqbnvhi...";
+
+        public static string GenerateToken(string email, string role)
+        {
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Secret));
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Email, email), new Claim(ClaimTypes.Role, role) }),
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
+            return handler.WriteToken(token);
+        }
+    }
+}
