67687d19132fddf6d5cff677e495338a14fb6f89
Usman Afzal Khan <usmanafzalkhan01@gmail.com>
2024-04-07 16:37:01 -0400

    feat: add update category functionality and get all category

--- brew-master-pro/Controllers/CategoryController.cs
+++ brew-master-pro/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.InteropServices.ComTypes;
 using System.Web.Http;
 
 namespace brew_master_pro.Controllers
@@ -37,6 +38,52 @@ namespace brew_master_pro.Controllers
             {
                 return Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
             }
+        }
+
+        [HttpGet, Route("getAllCategory")]
+        [CustomAuthenticationFilter]
+        public HttpResponseMessage GetAllCategory()
+        {
+            try 
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, db.Categories.ToList());
+            }
+            catch (Exception ex) 
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [HttpPost, Route("updateCategory")]
+        [CustomAuthenticationFilter]
+        public HttpResponseMessage UpdateCategory(Category category) 
+        {
+
+            try 
+            {
+                var token = Request.Headers.GetValues("authorization").First();
+                TokenClaim tokenClaim = TokenManager.ValidateToken(token);
+                if (tokenClaim.Role != "admin")
+                {
+                    return Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+                Category categoryObj = db.Categories.Find(category.id);
+                if (categoryObj == null)
+                {
+                    response.message = "Category ID is not found";
+                    return Request.CreateResponse(HttpStatusCode.OK, response);
+                }
+                categoryObj.name = category.name;
+                db.Entry(categoryObj).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                response.message = "Category Updated Successfully";
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception ex) 
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        
         }
     }
 }
