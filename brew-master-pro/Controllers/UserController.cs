545b5ca8de7698fcaf7e3ad050fed331e4b2b4ef
Usman Afzal Khan <usmanafzalkhan01@gmail.com>
2024-04-07 09:17:49 -0400

    feat: add getAllUser method and check token

--- brew-master-pro/Controllers/UserController.cs
+++ brew-master-pro/Controllers/UserController.cs
@@ -41,16 +41,16 @@ namespace brew_master_pro.Controllers
         [HttpPost, Route("login")]
         public HttpResponseMessage Login([FromBody] User user)
         {
-            try 
+            try
             {
-                User existingUser = db.Users.Where(u =>(u.email == user.email && u.password == user.password)).FirstOrDefault();
-                if (existingUser != null) 
+                User existingUser = db.Users.Where(u => (u.email == user.email && u.password == user.password)).FirstOrDefault();
+                if (existingUser != null)
                 {
-                    if(existingUser.status == "true")
+                    if (existingUser.status == "true")
                     {
                         return Request.CreateResponse(HttpStatusCode.OK, new { token = TokenManager.GenerateToken(existingUser.email, existingUser.role) });
                     }
-                    else 
+                    else
                     {
                         return Request.CreateResponse(HttpStatusCode.Unauthorized, new { message = "Wait for Admin Approval" });
                     }
@@ -60,10 +60,42 @@ namespace brew_master_pro.Controllers
                     return Request.CreateResponse(HttpStatusCode.Unauthorized, new { message = "Incorrect Username or Password" });
                 }
             }
-            catch (Exception e) 
+            catch (Exception e)
             {
                 return Request.CreateResponse(HttpStatusCode.InternalServerError, e);
             }
         }
+        [HttpGet, Route("checkToken")]
+        [CustomAuthenticationFilter]
+        public HttpResponseMessage checkToken()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new { message = "true" });
+        }
+
+        [HttpGet, Route("getAllUser")]
+        [CustomAuthenticationFilter]
+        public HttpResponseMessage GetAllUser() 
+        {
+            try 
+            {
+                var token = Request.Headers.GetValues("authorization").First();
+                TokenClaim tokenClaim = TokenManager.ValidateToken(token);
+                if(tokenClaim.Role != "admin")
+                {
+                    return Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+                var result = db.Users.Select(u => new { u.id, u.name, u.contactNumber, u.email, u.status, u.role }).Where(x => (x.role == "user")).ToList();
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
     }
 }
+
+
+
+
+
